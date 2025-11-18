e739bc567d016b69f507e67ed400787961504640
Chinmay Jape <chinmayjape2@gmail.com>
2025-11-18 14:20:47 +0530

    added docker

--- src/server.js
+++ src/server.js
@@ -1,13 +1,4 @@
 import app from "./app.js";
 
-const PORT = 8080;
-
-app.use((req, res, next) => {
-  console.log("🔥 Incoming Request:", req.method, req.url);
-  console.log("Headers:", req.headers);
-  next();
-});
-
-app.listen(PORT, () => {
-  console.log(`server running at ${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log("Server running on " + PORT));
